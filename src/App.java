99b4f3aca356cd300bd730424ad7c408457187e5
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-04-11 16:29:23 -0300

    Criação da classe responsável por criar todo tipo de personagem

--- src/App.java
+++ src/App.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 
 public class App {
     private static SelecaoPersonagens selecaoPersonagens = new SelecaoPersonagens();
-    private static ArrayList<Personagem> personagens = new ArrayList<Personagem>();
+
     public static void main(String[] args) {
         // Teste
+        ArrayList<Personagem> personagens = new ArrayList<Personagem>();
+        ArrayList<Npc> npcs = new ArrayList<Npc>();
+
         System.out.println("===== RMI HEROES =====");
         if(selecaoPersonagens.mostrarOpcoesIniciais() == 2) {
             System.out.println("Jogo finalizado!");
@@ -15,22 +18,23 @@ public class App {
         } else {
             int idClassePersonagem = selecaoPersonagens.selecionarPersonagem();
             if(idClassePersonagem == 1) {
-                Arqueiro arqueiro = new Arqueiro(59, 640, 26);
+                Arqueiro arqueiro = CriadorDePersonagem.criarArqueiro();
                 personagens.add(arqueiro);
                 System.out.println(arqueiro.toString());
             } else if(idClassePersonagem == 2) {
-                Guerreiro guerreiro = new Guerreiro(64, 652, 39);
+                Guerreiro guerreiro = CriadorDePersonagem.criarGuerreiro();
                 personagens.add(guerreiro);
                 System.out.println(guerreiro.toString());
             } else if(idClassePersonagem == 3) {
-                Mago mago = new Mago(53, 590, 21);
+                Mago mago = CriadorDePersonagem.criarMago();
                 personagens.add(mago);
                 System.out.println(mago.toString());
             } else if(idClassePersonagem == 4) {
-                Suporte suporte = new Suporte(47, 595, 29);
+                Suporte suporte = CriadorDePersonagem.criarSuporte();
                 personagens.add(suporte);
                 System.out.println(suporte.toString());
             }
         }
+        npcs = CriadorDePersonagem.criarNpcs();
     }
 }
